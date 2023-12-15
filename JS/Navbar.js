39be44d2c44cdf2ca37b81c427bdf4ca08939355
Zainqalandar <zainqlandar@gmail.com>
2023-12-15 18:43:56 +0500

    Add Projects & Contact Page with Resposive Layout

---
+++ JS/Navbar.js
@@ -0,0 +1,11 @@
+function navOpen(params) {
+    console.log('Navbar Open')
+    let navopen = document.getElementsByClassName("for__semall")[0]
+    navopen.style = 'display: block;'
+}
+function navClose() {
+    console.log('Nabar Close')
+    let navclose = document.getElementsByClassName("for__semall")[0]
+    navclose.style = 'display: none;'
+
+}
